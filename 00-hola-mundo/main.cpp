efa242d29421425255cf15fc011d78bc44990459
Silvina Navarro <silvinanvarro.lab@gmail.com>
2025-10-27 21:40:28 -0300

    00:Hola mundo

---
+++ 00-hola-mundo/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> //trae el cout (salida) y cin (entrada)
+using namespace std; //evita que escribamos std::cout
+
+int main (){ //punto de inicio de nuestro programa
+  cout << "Hola soy Silvi desde C++" << endl; //muestra el texto
+  return 0; // 0= termino bien
+}
